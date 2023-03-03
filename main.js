8e9ff064eddf9f568966ce96f0ed0e28aa504495
anabelcastillo <anabelcas@gmail.com>
2023-03-03 17:12:24 +0100

    "Exercici 3"

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 "use strict";
-const API_URL_JOKE = "https://icanhazdadjoke.com/";
+const API_URL_JOKE = "https://icanhazdadjoke.com/"; // URL de la API
 const header = {
     headers: {
         Accept: 'application/json'
     }
 };
-const divJoke = document.getElementById("show-joke");
-const btnNextJoke = document.getElementById("btn-next");
+// Seleccionamos los elementos del DOM
+const btnNext = document.getElementById("btn-next"); // Botón para obtener el siguiente chiste
+const jokeText = document.getElementById("joke-text"); // Texto donde se mostrará el chiste
+const scoreBtns = document.getElementById("score-buttons"); // Div donde se mostrarán los botones de puntuación
+const reportJokes = [];
+const d = new Date();
+let text = d.toISOString();
+// Función para obtener el chiste y mostrarlo en el DOM
 function getJoke() {
     fetch(API_URL_JOKE, header)
         .then(response => response.json())
-        .then(data => {
-        divJoke.innerHTML = data.joke;
-        console.log(data.joke);
+        .then((data) => {
+        jokeText.innerHTML = data.joke; // Muestra el chiste
+        scoreBtns.style.display = "block"; // Mostramos los botones de puntuación
     })
         .catch(error => console.log(error));
+}
+// Función para actualizar la puntuación del chiste
+function userScore(score) {
+    const currentJoke = reportJokes.find(joke => joke.joke === jokeText.innerHTML);
+    if (currentJoke) {
+        currentJoke.score = score;
+    }
+    else {
+        reportJokes.push({
+            joke: jokeText.innerHTML,
+            score: score,
+            date: text,
+        });
+    }
+    console.log(reportJokes); // Mostramos el array reportJokes en la consola
 }
--- main.ts
+++ main.ts
@@ -1,19 +1,42 @@
-const API_URL_JOKE:string = "https://icanhazdadjoke.com/";
-const header:object = {
+const API_URL_JOKE:string = "https://icanhazdadjoke.com/"; // URL de la API
+const header: object = {
   headers: {
     Accept: 'application/json'
   }
 };
 
-const divJoke: HTMLElement | null = document.getElementById("show-joke");
-const btnNextJoke: HTMLElement | null = document.getElementById("btn-next");
+// Seleccionamos los elementos del DOM
+const btnNext: HTMLElement | null = document.getElementById("btn-next"); // Botón para obtener el siguiente chiste
+const jokeText: HTMLElement | null = document.getElementById("joke-text"); // Texto donde se mostrará el chiste
+const scoreBtns: HTMLElement | null = document.getElementById("score-buttons"); // Div donde se mostrarán los botones de puntuación
 
-function getJoke() {
+const reportJokes: any[] = [];
+const d = new Date();
+let text = d.toISOString();
+
+// Función para obtener el chiste y mostrarlo en el DOM
+function getJoke(): void {
   fetch(API_URL_JOKE, header)
   .then(response => response.json())
-  .then(data => {
-    divJoke.innerHTML = data.joke;
-    console.log(data.joke);
+  .then((data: { joke: string }) => {
+    jokeText.innerHTML = data.joke; // Muestra el chiste
+    scoreBtns.style.display = "block"; // Mostramos los botones de puntuación
   })
   .catch(error => console.log(error));
+}
+
+// Función para actualizar la puntuación del chiste
+function userScore(score: number): void {
+  const currentJoke = reportJokes.find(joke => joke.joke === jokeText.innerHTML);
+
+  if(currentJoke) {
+    currentJoke.score = score;
+  } else {
+    reportJokes.push({
+      joke: jokeText.innerHTML,
+      score: score,
+      date: text,
+    });
+  }
+  console.log(reportJokes); // Mostramos el array reportJokes en la consola
 }
